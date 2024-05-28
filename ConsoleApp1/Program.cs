2be4328a7e12313b85b871717baf9a828c6385af
Python <svetloewill@mail.ru>
2024-05-28 17:08:14 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите свое имя: ");
+        var name = Console.ReadLine();       // вводим имя
+        Console.WriteLine($"Привет {name}");
+    }
+}
