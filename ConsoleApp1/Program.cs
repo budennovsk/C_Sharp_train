3e03f67f7d2087344e5a131888b5ee743638c19b
Python <svetloewill@mail.ru>
2024-05-29 13:16:16 +0300

    2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,25 @@
-﻿internal class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.Write("Введите свое имя: ");
-        var name = Console.ReadLine();       // вводим имя
-        Console.WriteLine($"Привет {name}");
-    }
+﻿// internal class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         Console.Write("Введите свое имя: ");
+//         var name = Console.ReadLine();       // вводим имя
+//         Console.WriteLine($"Привет {name}");
+//     }
+// }
+// ReSharper disable once SuggestVarOrType_BuiltInTypes
+// int first_var = 22;
+// Console.WriteLine(first_var);
+// string first_var = "REFERFR";
+// Console.WriteLine(first_var);
+
+int num =0;
+
+int Incrementnum(ref int n)
+{   
+    return n+=5;
 }
+Console.WriteLine(num);
+var res = Incrementnum(ref num);
+Console.WriteLine(num);
+Console.WriteLine(res);
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be4328a7e12313b85b871717baf9a828c6385af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
